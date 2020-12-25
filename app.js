10a01e60c43b1959d1f9dd05f3837a3be2fa3fa3
sipesino <richardpesino@gmail.com>
2020-12-25 22:57:53 +0800

    5th

---
+++ app.js
@@ -0,0 +1,11 @@
+document.querySelector(".addBtn").addEventListener('click', function () {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.cancel').addEventListener('click', function () {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.querySelector('.add').addEventListener('click', function () {
+    console.log('Pikoy');
+});
